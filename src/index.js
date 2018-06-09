dd4538a07427efb2da2e7eedd7fdfd7b2b098a46
Kota Nonaka <kota.kaicho@gmail.com>
2018-06-09 12:39:13 +0900

    Initialize project.

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import ApolloClient from 'apollo-client';
+import { HttpLink, InMemoryCache } from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://api.github.com/graphql',
+    headers: {
+      Authorization: `bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`
+    }
+  }),
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'));
+registerServiceWorker();
