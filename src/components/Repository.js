8f81fe7c5ef537b939451f0ee88f277d07841713
Kota Nonaka <kota.kaicho@gmail.com>
2018-06-12 19:05:03 +0900

    Modified style.

--- src/components/Repository.js
+++ src/components/Repository.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import Card from '@material-ui/core/Card'
+import { withStyles } from '@material-ui/core/styles';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { Icon } from '@material-ui/core';
 
 const Repository = ({repository}) => (
-  <div>
-    <dl>
-      <dt>Name</dt>
-      <dd>{repository.name}</dd>
-      <dt>Owner</dt>
-      <dd>{repository.owner.login}</dd>
-      <dt>Star count</dt>
-      <dd>{repository.stargazers.totalCount}</dd>
-    </dl>
-  </div>
+  <Card>
+    <CardContent>
+      <Typography variant="headline" component="h2">
+        {repository.owner.login} / {repository.name}
+      </Typography>
+      <Typography color="secondary">
+        <Icon>star</Icon>{repository.stargazers.totalCount} Stars
+      </Typography>
+    </CardContent>
+  </Card>
 )
 
 export default Repository;
